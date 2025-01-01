7ea8c56e581fb193d686e5f8f7757a933a9bb67e
Cengiz Poyraz <cengizp@hotmail.com>
2025-01-01 13:03:01 +0300

    gms path fix

--- gms_util.py
+++ gms_util.py
@@ -1,7 +1,10 @@
 import os
 GMS_HOME = os.getenv('GMS_HOME')
 def get_gms_path(*paths_to_join):
-    joined_path = GMS_HOME
-    for path in paths_to_join:
+    if paths_to_join[0].startswith(GMS_HOME):        
+        joined_path = paths_to_join[0]
+    else:
+        joined_path = GMS_HOME
+    for path in paths_to_join[1:]:
         joined_path = os.path.join(joined_path, path.lstrip('/'))
     return joined_path
