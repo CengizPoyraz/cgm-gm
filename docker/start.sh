ae78266f3aec7b547b9fc3f6844b3f07c14f7f19
Cengiz Poyraz <cengizp@hotmail.com>
2025-01-09 14:42:10 +0300

    start fix

--- docker/start.sh
+++ docker/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --mount type=bind,src=~/docker/shared,target=/home/root/host --gpus all cpoyraz/gms:v3.4 bash
+docker run -it --mount type=bind,src=/home/gms/docker/shared,target=/home/root/host --gpus all cpoyraz/gms:v3.4 bash
